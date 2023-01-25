3303d9149712660235466d4b3cb68acbe96feb90
EmyRukundo <39124680+EmyRukundo@users.noreply.github.com>
2023-01-25 17:01:45 +0200

    first commit

---
+++ src/main/java/dasPortefeuillecom/example/dasPortefeuille/DasPortefeuilleApplication.java
@@ -0,0 +1,13 @@
+package dasPortefeuillecom.example.dasPortefeuille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasPortefeuilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasPortefeuilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dasPortefeuillecom/example/dasPortefeuille/DasPortefeuilleApplicationTests.java
@@ -0,0 +1,13 @@
+package dasPortefeuillecom.example.dasPortefeuille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasPortefeuilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
