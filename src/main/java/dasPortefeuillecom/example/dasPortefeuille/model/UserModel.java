60d60936bf6af0ad4912160e08cb41d487b2b4c4
EmyRukundo <39124680+EmyRukundo@users.noreply.github.com>
2023-01-25 21:44:49 +0200

    connecting to db

---
+++ src/main/java/dasPortefeuillecom/example/dasPortefeuille/model/UserModel.java
@@ -0,0 +1,29 @@
+package dasPortefeuillecom.example.dasPortefeuille.model;
+
+
+import jakarta.persistence.*;
+
+@Entity(name="users")
+
+public class UserModel {
+
+    @Id
+    @SequenceGenerator(name = "users_sequence", sequenceName = "users_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_sequence")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String firstName;
+
+    @Column(nullable = false, length = 20)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+}
